bf7357f6025cb871c1c34a64fc616f87e9b1bce3
Tyuiu.SeledkovNP <Tyuiu.SeledkovNP@outlook.com>
2024-09-11 20:27:18 +0500

    + Task1

---
+++ Tyuiu.SeledkovNikita.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SeledkovNikita.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Word");
+            string name = "Gar";
+                int age = 42;
+            Console.WriteLine("Здраствуйте меня не зовут " + name + " мне не " + age + " столетей!");
+            Console.ReadKey();
+        }
+    }
+}
