7106824e9664100dceefbb70b81450e88e0e1ccc
Nikita-Seledkov <nikita_seledkov@mail.ru>
2024-09-16 11:27:09 +0500

    Add files via upload

---
+++ Tyuiu.SeledkovNP.Sprint0/Tyuiu.SeledkovNikita.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SeledkovNikita.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Word");                                                                // текст первый
+            string name = "Призрак";                                                                        // Имя
+                int age = 42;                                                                              // указанные года
+            Console.WriteLine("Здраствуйте меня не зовут " + name + " мне не " + age + " столетей!");     // последный текс
+            Console.ReadKey();                                                                           // ожидания ответа
+        }
+    }
+}
